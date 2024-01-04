6dbe30a2dca7dd55d20b26643cdcc384970590ab
Kalyankal14 <134216294+Kalyankal14@users.noreply.github.com>
2024-01-04 16:18:54 +0000

    cors error

--- coursera-app/app/page.tsx
+++ coursera-app/app/page.tsx
@@ -1,37 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-import CourseList from "@/ui/Courses/CourseList";
 import SearchBar from "@/ui/components/SearchBar";
 
-async function getData() {
-  const prismaData = new PrismaClient();
-  const courseList = await prismaData.courses.findMany({});
-  const usersList = await prismaData.users.findMany({});
-  return {
-    allCourses : courseList,
-    allUsers : usersList
-  } 
-}
-const searchValue = async (text : any) => {
-  'use server'
-  text = text ? text : '';
-  const prismaData = new PrismaClient();
-  const courseList = await prismaData.courses.findMany({
-    where:{
-      title : {
-        contains : text || ''
-      }
-    }
-  });
-return courseList
-}
 export default async function Home() {
-  const data = await getData();
+  const searchValue = async (text : any) => {
+    'use server'
+    text = text ? text : '';
+    const prismaData = new PrismaClient();
+    const courseList = await prismaData.courses.findMany({
+      where:{
+        title : {
+          contains : text || ''
+        }
+      }
+    });
+  return courseList
+  }
   return (
     <>
     <h1> Home </h1>
     <SearchBar searchCallback={searchValue}/>
-    <CourseList list={data.allCourses}/>
-        <pre> {JSON.stringify(data,null,2)}</pre>
     </>
       )
 }
--- coursera-app/ui/components/SearchBar.tsx
+++ coursera-app/ui/components/SearchBar.tsx
@@ -3,45 +3,39 @@
 import React, {useState, useEffect} from 'react';
 import { Input, Button } from '@/ui';
 import { useDebounce } from 'use-debounce';
+import { Loading } from './loading';
 
 export default function SearchBar({searchCallback} : any){
     const [search, setSearch] = useState('');
+    const [courses,setCourses] = useState([]);
     const [loading ,setLoading] = useState(false);
-    
-    // const getCourses = (text:string) => {
-    //     const API_URL = `/api/courses?query=${text}`;
-    //     fetch(API_URL)
-    //         .then(res => res.json())
-    //         .then(console.log)
-    // }
+
     const [debouncedValue] = useDebounce(search, 500);
 
     useEffect(()=>{
-        setLoading(true)
+        //setLoading(true)
         const timer = setTimeout(()=>{
-            searchCallback(search)
+            searchCallback(search).then((res:any)=>{
+                console.log(res,'::response::')
+                setCourses(res);
+                setLoading(false);
+            })
         },500)
+        return ()=>clearInterval(timer);
         
     },[debouncedValue])
 
-    // const onSearch = () => {
-    //     searchCallback(search);
-    //     setSearch('');
-    // }
+    
     return <>
     <div className='w-1/2 ml-auto mr-auto m-4'>
           <Input 
-        //  className='w-1/2 rounded'
           crossOrigin={''} 
           label="Search here..." 
           value={search}
           onChange={(e)=>setSearch(e.target.value)}
           />
           </div>
-          {/* <Button 
-          placeholder={''}
-          onClick={onSearch}> Search </Button>
-          <p>Search word : {search}</p> */}
+         {loading ? <Loading /> : <> Not loading </>}
 
     </>
 }
